78d5c805b33f48a1295b1b1bea63483aa0d8c759
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2023-02-21 10:53:36 +0100

    Use tags instead of names in noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,23 +1,23 @@
 import nox
 
 
-@nox.session(name="lint", venv_backend="none")
+@nox.session(tags=["lint"], venv_backend="none")
 def fmt_check(s: nox.Session) -> None:
     s.run("isort", "--check", ".")
     s.run("black", "--check", ".")
 
 
-@nox.session(name="lint", venv_backend="none")
+@nox.session(tags=["lint"], venv_backend="none")
 def lint(s: nox.Session) -> None:
     s.run("pflake8", "--color", "always", "src", "tests")
 
 
-@nox.session(name="lint", venv_backend="none")
+@nox.session(tags=["lint"], venv_backend="none")
 def type_check(s: nox.Session) -> None:
     s.run("mypy", "src", "noxfile.py")
 
 
-@nox.session(name="test", venv_backend="none")
+@nox.session(tags=["test"], venv_backend="none")
 def test(s: nox.Session) -> None:
     s.run("pytest", "--cov", "--junitxml=test_result.xml")
     s.run("coverage", "report")
