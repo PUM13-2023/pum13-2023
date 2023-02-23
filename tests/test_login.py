83903266e1e023ed449b9844ad163102566a2f5e
woshyy <chrwa634@student.liu.se>
2023-02-23 17:02:17 +0100

    rewrote it so it is using pytest class

--- tests/test_login.py
+++ tests/test_login.py
@@ -38,137 +38,123 @@ class LoginSettings():
     SLEEP_TIME_REFRESH = 5
 
 
-@pytest.fixture
-def website_url():
-    return LoginSettings.START_PAGE_URL
+class TestLogin():
 
+    @pytest.fixture
+    def browser_driver(self):
+        match LoginSettings.WEB_BROWSER:
+            case WebBrowser.FIREFOX:
+                self.driver = webdriver.Firefox()
+            case WebBrowser.CHROME:
+                self.driver = webdriver.Chrome()
+        self.driver.get(LoginSettings.START_PAGE_URL)
+        yield
+        self.driver.close()
 
-@pytest.fixture
-def web_browser():
-    return LoginSettings.WEB_BROWSER
+    @pytest.mark.usefixtures('browser_driver')
+    def test_unsuccessful_login(self):
 
+        # Try logging in with wrong password and username
+        self.try_login(LoginSettings.WRONG_USERNAME,
+                       LoginSettings.WRONG_PASSWORD,
+                       self.driver)
+        self.check_login_error_pop_up(self.driver)
 
-@pytest.fixture
-def browser_driver(web_browser, website_url):
-    match web_browser:
-        case WebBrowser.FIREFOX:
-            driver = webdriver.Firefox()
-        case WebBrowser.CHROME:
-            driver = webdriver.Chrome()
-    driver.get(website_url)
-    return driver
+        # Try logging in with wrong password
+        self.try_login(LoginSettings.USERS_USERNAME,
+                       LoginSettings.WRONG_PASSWORD,
+                       self.driver)
+        self.check_login_error_pop_up(self.driver)
 
+        # Try logging in with wrong username
+        self.try_login(LoginSettings.WRONG_USERNAME,
+                       LoginSettings.USERS_PASSWORD,
+                       self.driver)
+        self.check_login_error_pop_up(self.driver)
+    
+    @pytest.mark.usefixtures('browser_driver')
+    def test_successfull_login(self):
+        self.try_login(LoginSettings.USERS_USERNAME,
+                       LoginSettings.USERS_PASSWORD,
+                       self.driver)
+        # Check that we are still in the homepage after login in
+        assert (self.is_in_home_page(self.driver))
 
-def test_unsuccessful_login(browser_driver):
-    username_text_field_exist = check_username_text_field(browser_driver)
-    if (not username_text_field_exist):
-        browser_driver.close()
-    assert username_text_field_exist, "There is no username text field"
-    # Try logging in with wrong password and username
-    if not try_login(LoginSettings.WRONG_USERNAME,
-                     LoginSettings.WRONG_PASSWORD,
-                     browser_driver):
-        return
-    check_login_error_pop_up(browser_driver)
+        # Check that we are still  in the homepage after
+        # refreshing in the login page.
+        self.driver.refresh()
+        assert (self.is_in_home_page(self.driver))
 
-    # Try logging in with wrong password
-    try_login(LoginSettings.USERS_USERNAME,
-              LoginSettings.WRONG_PASSWORD,
-              browser_driver)
-    check_login_error_pop_up(browser_driver)
+        self.driver.close()
 
-    # Try logging in with wrong username
-    try_login(LoginSettings.WRONG_USERNAME,
-              LoginSettings.USERS_PASSWORD,
-              browser_driver)
-    check_login_error_pop_up(browser_driver)
+    # @pytest.mark.usefixtures('browser_driver')
+    # def test_logout(self):
+    #     sleep(2)
+    #     # try_login(LoginSettings.USERS_USERNAME,
+    #     #           LoginSettings.USERS_PASSWORD,
+    #     #           browser_driver)
+    #     # Check that we are still in the homepage after login in
+    #     # assert (is_in_home_page(browser_driver))
 
+    #     self.driver.close()
 
-# def test_successfull_login(browser_driver):
-#     try_login(LoginSettings.USERS_USERNAME,
-#               LoginSettings.USERS_PASSWORD,
-#               browser_driver)
-
-#     # Check that we are still in the homepage after login in
-#     assert (is_in_home_page(browser_driver))
-
-#     # Check that we are still  in the homepage after
-#     # refreshing in the login page.
-#     browser_driver.refresh()
-#     assert (is_in_home_page(browser_driver))
-
-#     browser_driver.close()
-
-
-# def test_logout(browser_driver):
-#     sleep(5)
-#     # try_login(LoginSettings.USERS_USERNAME,
-#     #           LoginSettings.USERS_PASSWORD,
-#     #           browser_driver)
-#     # Check that we are still in the homepage after login in
-#     # assert (is_in_home_page(browser_driver))
-
-#     browser_driver.close()
-
-def check_username_text_field(driver: webdriver):
-    username_text_field_candidates: list(WebElement) = driver.find_elements(
-        By.NAME, LoginSettings.NAME_USERNAME_ELEMENT)
-    n_username_text = len(username_text_field_candidates)
-    return n_username_text == 1
-
-
-def try_login(username, password, driver: webdriver) -> bool:
-    # Finding the username text field and filling out the text field with
-    # the username
-    username_text_field_candidates: list(WebElement) = driver.find_elements(
-        By.NAME, LoginSettings.NAME_USERNAME_ELEMENT)
-    n_username_text = len(username_text_field_candidates)
-    print(n_username_text)
-    if (n_username_text != 1):
-        driver.close()
+    def check_username_text_field(self, driver: webdriver):
+        username_text_field_candidates: list(WebElement) = driver.find_elements(
+            By.NAME, LoginSettings.NAME_USERNAME_ELEMENT)
         n_username_text = len(username_text_field_candidates)
-        assert n_username_text != 1, "Could not find the username text field"
-        return False
-    username_text_field: WebElement = username_text_field_candidates[0]
-    username_text_field.clear()
-    username_text_field.send_keys(username)
-    sleep(LoginSettings.SLEEP_TIME_USERNAME_FIELD)
+        return n_username_text == 1
 
-    # Finding the password text field and filling out the text field with
-    # the username
-    password_text_field_candidates: list(WebElement) = driver.find_elements(
-        By.NAME, LoginSettings.NAME_PASSWORD_ELEMENT)
-    if (len(password_text_field_candidates) != 1):
-        driver.close()
-        assert (len(username_text_field_candidates) != 1)
-    password_text_field: WebElement = password_text_field_candidates[0]
-    password_text_field.clear()
-    password_text_field.send_keys(password)
-    sleep(LoginSettings.SLEEP_TIME_PASSWORD_FIELD)
+    def try_login(self, username, password, driver) -> bool:
+        # Finding the username text field and filling out the text field with
+        # the username
+        name_text_fields: list(WebElement) = driver.find_elements(
+            By.NAME, LoginSettings.NAME_USERNAME_ELEMENT)
+        assert not len(name_text_fields) == 0, '0 username text field'
+        assert not len(name_text_fields) > 1, 'More than 1 name text field'
 
-    # Finding the login button and click it!
-    login_button: WebElement = driver.find_element(
-        By.NAME, LoginSettings.ID_LOGIN_ELEMENT)
-    assert (login_button is not None)
-    login_button.click()
-    sleep(LoginSettings.SLEEP_TIME_LOGIN_BUTTON)
+        username_text_field: WebElement = name_text_fields[0]
+        username_text_field.clear()
+        username_text_field.send_keys(username)
+        sleep(LoginSettings.SLEEP_TIME_USERNAME_FIELD)
 
+        # Finding the password text field and filling out the text field with
+        # the username
+        pass_fields: list(WebElement) = driver.find_elements(
+            By.NAME, LoginSettings.NAME_PASSWORD_ELEMENT)
+        assert not len(pass_fields) == 0, '0 password text field'
+        assert not len(pass_fields) > 1, 'More than 1 password text field'
 
-def check_login_error_pop_up(driver: webdriver) -> None:
-    # Check if the login error pop up exist and then refresh the site and
-    # check that it does not exist.
-    pop_up_element: WebElement = driver.find_element(
-        By.ID, LoginSettings.ID_POP_UP_ELEMENT)
-    assert (pop_up_element is not None)
-    driver.refresh()
-    sleep(LoginSettings.SLEEP_TIME_REFRESH)
-    pop_up_element: WebElement = driver.find_element(
-        By.ID, LoginSettings.ID_POP_UP_ELEMENT)
-    assert (pop_up_element is None)
+        password_text_field: WebElement = pass_fields[0]
+        password_text_field.clear()
+        password_text_field.send_keys(password)
+        sleep(LoginSettings.SLEEP_TIME_PASSWORD_FIELD)
+
+        # Finding the login button and click it!
+        login_buttons: WebElement = driver.find_elements(
+            By.NAME, LoginSettings.ID_LOGIN_ELEMENT)
+        assert not len(login_buttons) == 0, '0 login button'
+        assert not len(login_buttons) > 1, ' x > 1 login buttons'
+
+        login_button = login_buttons[0]
+        login_button.click()
+        sleep(LoginSettings.SLEEP_TIME_LOGIN_BUTTON)
 
 
-def is_in_home_page(driver: webdriver) -> None:
-    # Check if we are in the home page.
-    in_home_page: bool = driver.find_element(
-        By.ID, LoginSettings.ID_LOGOUT_ELEMENT) is not None
-    return in_home_page
+    def check_login_error_pop_up(driver: webdriver) -> None:
+        # Check if the login error pop up exist and then refresh the site and
+        # check that it does not exist.
+        pop_up_element: WebElement = driver.find_element(
+            By.ID, LoginSettings.ID_POP_UP_ELEMENT)
+        assert (pop_up_element is not None)
+        driver.refresh()
+        sleep(LoginSettings.SLEEP_TIME_REFRESH)
+        pop_up_element: WebElement = driver.find_element(
+            By.ID, LoginSettings.ID_POP_UP_ELEMENT)
+        assert (pop_up_element is None)
+
+
+    def is_in_home_page(driver: webdriver) -> None:
+        # Check if we are in the home page.
+        in_home_page: bool = driver.find_element(
+            By.ID, LoginSettings.ID_LOGOUT_ELEMENT) is not None
+        return in_home_page
