0b831b43085a3f81d2df9219e103249ef56269e4
Tobias Elfstrand <elfstrandtobias@gmail.com>
2023-02-23 11:35:34 +0100

    fixing warning for pytest

--- src/dashboard/pages/index/index.py
+++ src/dashboard/pages/index/index.py
@@ -2,10 +2,10 @@ import dash
 from dash import html
 from dash.dependencies import Component
 
-from dashboard.components import test_component
+from dashboard.components import experimental_component
 
 dash.register_page(__name__, path="/")
 
 
 def layout() -> Component:
-    return html.Div(children=[html.H1(children="Index page!"), test_component()])
+    return html.Div(children=[html.H1(children="Index page!"), experimental_component()])
