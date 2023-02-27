bafd1b50798a16a5af90253d7b3eab6810ed584a
Axel Matstoms <axel.matstoms@gmail.com>
2023-02-27 11:14:35 +0100

    Update db model, tests to use mongoengine
    * Update db model to use mongoengine
    * Since mongoengine uses a global state, an alias parameter has been
      introduced to the connect functions to manage simultaneous connections
      to different databases.
    * Update tests to correspond to updated functions

--- src/dashboard/models/db.py
+++ src/dashboard/models/db.py
@@ -2,60 +2,37 @@ import os
 from typing import Tuple
 
 from dotenv import load_dotenv
-from pymongo import MongoClient, errors
-from pymongo.database import Database
-
-from dashboard.models.db_types import DocumentType
+import mongoengine
 
 load_dotenv()
 
 
-def connect(server_url: str, db_name: str) -> Database[DocumentType] | None:
+def connect(server_url: str, db_name: str, alias: str) -> None:
     """Connect to a MongoDB database.
 
     Args:
         server_url (str): the MongoDB URL to connnect to.
         db_name (str): the database object to retrieve.
-
-    Returns:
-        Database[DocumentType] | None: A Database object if a connection
-        could be established, otherwise returns None.
     """
-    try:
-        client: MongoClient[DocumentType] = MongoClient(server_url)
-    except errors.ConnectionFailure:
-        return None
-
-    db = client[db_name]
-
-    return db
+    mongoengine.connect(alias=alias, host=server_url, name=db_name)
 
 
 def get_connection_params(url_env_name: str, db_env_name: str) -> Tuple[str, str]:
     server_url = os.getenv(url_env_name)
     db_name = os.getenv(db_env_name)
     if not server_url or not db_name:
-        raise EnvironmentError(
-            "Can't find environment variables "
-            + f"{url_env_name}:{str(server_url)} or {db_env_name}:{db_name}"
-        )
+        raise EnvironmentError(f"Can't find environment variables {url_env_name} or {db_env_name}")
 
     return (server_url, db_name)
 
 
-def connect_data_db() -> Database[DocumentType] | None:
-    connection_params = get_connection_params("DATA_DB_URL", "DATA_DB_NAME")
-    server_url = connection_params[0]
-    db_name = connection_params[1]
+def connect_data_db(alias: str) -> None:
+    server_url, db_name = get_connection_params("DATA_DB_URL", "DATA_DB_NAME")
 
-    db = connect(server_url, db_name)
-    return db
+    connect(server_url, db_name, alias)
 
 
-def connect_user_db() -> Database[DocumentType] | None:
-    connection_params = get_connection_params("USER_DB_URL", "USER_DB_NAME")
-    server_url = connection_params[0]
-    db_name = connection_params[1]
+def connect_user_db(alias: str) -> None:
+    server_url, db_name = get_connection_params("USER_DB_URL", "USER_DB_NAME")
 
-    db = connect(server_url, db_name)
-    return db
+    connect(server_url, db_name, alias)
