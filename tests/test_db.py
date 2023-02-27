67675b051742900249d947c5dbf350cfc2160ff1
Axel Matstoms <axel.matstoms@gmail.com>
2023-02-27 09:31:54 +0100

    Fix test_connection_established
    * Use is not None instead of assert conn.

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,5 +3,5 @@ from dashboard.models import db
 
 def test_connection_established() -> None:
     data_client = db.connect_data_db()
-    assert data_client
+    assert data_client is not None
     # client_user = db.connect_user_db()
