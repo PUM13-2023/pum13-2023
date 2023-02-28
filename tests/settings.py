f40512e11ef0c9684c909722c4af60db93fc3a5c
Christopher Wåtz <Christopher.waatz@gmail.com>
2023-02-28 11:00:10 +0100

    Test creating (#31)
    * Change dependabot target branch
    
    * started working on the login test
    
    * Done test login
    
    * Continued working with the test. Added a test for uncessfull login.
    
    * going to rebase from dev
    
    * Change dependabot target branch
    
    * started working on the login test
    
    * Done test login
    
    * Continued working with the test. Added a test for uncessfull login.
    
    * going to rebase from dev
    
    * Just moving workspace to the home pc
    
    * rewrote it so it is using pytest class
    
    * Made a test that could test peoples computer with selenium and pytest
    
    * pushing
    
    * Finish log out and did some code refractoring in test_login.py
    
    * Fixed a small error
    
    * added argument passing and wrote a readme that tells the other members in the project how to run the test
    
    * small changes
    
    * added speed multiplier to the test
    
    * fix the code according to black, isort, flake8
    
    * removed pytest.ini and moved all the test marks for pytest to pyproject.toml
    
    * Moved to contanst that are used in multiple files to settings.py whilst if the constants are only used on that file that it is written there
    
    * Moved some constants into settings.py and removed some constants from there and also added a flag so that running pytest would not run the youtube test
    
    * forgot to run black and isort and now everything should follow the code standard
    
    * Added a new line at the end of pyproject.toml and made small changes to read me
    
    * remove package.json
    
    * Added a line at the end of the pyproject.toml
    
    * removed pytest.ini and moved all the test marks for pytest to pyproject.toml
    
    Moved to contanst that are used in multiple files to settings.py whilst if the constants are only used on that file that it is written there
    
    Moved some constants into settings.py and removed some constants from there and also added a flag so that running pytest would not run the youtube test
    
    forgot to run black and isort and now everything should follow the code standard
    
    Added a new line at the end of pyproject.toml and made small changes to read me
    
    remove package.json
    
    Added a line at the end of the pyproject.toml
    
    ---------
    
    Co-authored-by: Andreas B <6439218+YouKnowBlom@users.noreply.github.com>

---
+++ tests/settings.py
@@ -0,0 +1,6 @@
+# Normally used constants
+START_PAGE_URL = "https://www.google.com/"
+USERS_USERNAME = "valid_user"
+USERS_PASSWORD = "valid_user_password"
+
+# Constants specifically for test_login
