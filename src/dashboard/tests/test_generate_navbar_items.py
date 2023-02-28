4f27e7c00374220a986d8923ddb73fbe0edbdef1
robin <drken_robin@hotmail.com>
2023-02-28 12:53:59 +0100

    Wrote four test cases for the generate_navbar_items function. These tests include checking that the correct name exists in the navbar and that the length of the generated navbar list is correct or incorrect

--- src/dashboard/tests/test_generate_navbar_items.py
+++ src/dashboard/tests/test_generate_navbar_items.py
@@ -1,34 +1,29 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
+from dashboard.components.navbar_component import generate_navbar_items, navbar_items
 
-PORT = 3030
-ADDRESS = '127.0.0.1'
-URL = f'{ADDRESS}:{str(PORT)}'
+ITEM_EXIST = "Dashboard"
+ITEM_NOT_EXIST = "😂😂😂"
+NAVBAR_ITEMS_COUNT = len(navbar_items)
 
 
 @pytest.mark.test_generate_navbar_items
 class TestGenerateNavbarItems:
-    @pytest.fixture(scope="session")
-    def speed_mult(self, request):
-        self.spd_mult = float(request.config.option.speedmult)
+    def test_item_not_exist(self):
+        navbar_list = generate_navbar_items(ITEM_NOT_EXIST)
+        assert navbar_list == 'Item to highlight does not exist in the navbar'
 
-    @pytest.fixture()
-    def browser_driver(self, request):
-        driver: webdriver
-        match request.config.option.browser:
-            case "chrome":
-                driver = webdriver.Chrome()
-            case "safari":
-                driver = webdriver.Safari()
-            case "edge":
-                driver = webdriver.Edge()
-            case "chromium":
-                driver = webdriver.ChromiumEdge()
-            case _:
-                driver = webdriver.Firefox()
-        yield driver
-        driver.close()
+    def test_item_exist(self):
+        navbar_list = generate_navbar_items(ITEM_EXIST)
+        assert navbar_list
+
+    def test_correct_number_items(self):
+        navbar_list = generate_navbar_items(ITEM_EXIST)
+        assert len(navbar_list) == NAVBAR_ITEMS_COUNT
+
+    def test_incorrect_number_items(self):
+        navbar_list = generate_navbar_items(ITEM_NOT_EXIST)
+        assert len(navbar_list) != NAVBAR_ITEMS_COUNT
+
+
 
 
