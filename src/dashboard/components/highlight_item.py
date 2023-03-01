9f7e38e3d677e7f55546650b15a5f4de20c06b18
robin <drken_robin@hotmail.com>
2023-03-01 16:18:39 +0100

    Added constant for class name

--- src/dashboard/components/highlight_item.py
+++ src/dashboard/components/highlight_item.py
@@ -1,11 +1,14 @@
 from dash import html
 from dash.dependencies import Component
 
+HIGHLIGHT_CLASSNAME = 'border-r-4 border-r-white text-white bg-[#777DF2]'
+
 
 def highlight_item(name: str) -> Component:
     return (
-        html.P(
+        html.A(
             name,
-            className='border-r-4 border-r-white text-white bg-[#777DF2]'
+            className=HIGHLIGHT_CLASSNAME,
+            href=name
         )
     )
