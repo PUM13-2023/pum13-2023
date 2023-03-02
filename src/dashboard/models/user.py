2af19debad9e13f55dcd33463c7439c4a8765493
Axel Matstoms <axel.matstoms@gmail.com>
2023-03-02 09:52:22 +0100

    Add user models
    + Add User, Dashboard, Diagram model stubs, which will need to be
      expanded.
    + Add tests to test user login, registry.

---
+++ src/dashboard/models/user.py
@@ -0,0 +1,67 @@
+"""
+Models related to user database.
+"""
+from mongoengine import (
+    Document,
+    EmbeddedDocument,
+    EmbeddedDocumentListField,
+    ListField,
+    ObjectIdField,
+    StringField,
+    ReferenceField
+)
+
+
+class Diagram(EmbeddedDocument):
+    """Diagram database mode.
+
+    A diagram stores all the information needed to reconstruct a Dast
+    diagram.
+
+    The current implementation is a stub, which will need to be
+    extended to store various metadata about the diagram.
+
+    Attributes:
+        data (DBRef): A DBRef to the data object which contains the
+            data to be plotted.
+    """
+    data = ReferenceField("data", dbref=True)
+
+
+class Dashboard(EmbeddedDocument):
+    """Dashboard database model.
+
+    A Dashboard stores all the data associated with reconstructing a
+    dashboard page.
+
+    Attributes:
+        authorized_users (list[User]): the list of users authorized to
+            access the dashboard.
+        diagrams (list[Diagram]): the diagrams which the dashboard
+            consists of.
+    """
+    authorized_users = ListField(ReferenceField("User"))
+    diagrams = EmbeddedDocumentListField(Diagram)
+
+
+class User(Document):
+    """User database model.
+
+    The current User implementation contains no account security and
+    is only used for testing purposes.
+
+    Attributes:
+        username (str): The users username, used to identify the user.
+        dashboards (list[Dashboard]): list of embedded dashboard
+            documents.
+    """
+    username = StringField()
+    dashboards = EmbeddedDocumentListField(Dashboard)
+
+
+def register_user(username: str) -> User:
+    pass
+
+
+def login_user(username: str) -> User:
+    pass
