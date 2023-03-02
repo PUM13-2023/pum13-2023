df524b33b36254fd1b4a6607aafc3c71b03ab5ea
robin <drken_robin@hotmail.com>
2023-03-02 19:16:13 +0100

    Fix linter and headless: Added headless run option to the login tests. Dependabot should be able pass the tests now. Fixed linter issues

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,16 +1,14 @@
 from .test_enum import WebBrowser
-import test_login
-
 
 # The link to the home website.
-WEBSITE = 'http://www.python.org'
+WEBSITE = "http://www.python.org"
 # The web_browser that would be used to test
 WEB_BROWSER = WebBrowser.FIREFOX
 
 
 def main():
-    test_login(WEB_BROWSER)
+    pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
