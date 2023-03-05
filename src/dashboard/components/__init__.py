3c4b4f46d8e76d1b5621743842e00c7037f63bdb
Axel Matstoms <axel.matstoms@gmail.com>
2023-03-05 17:13:45 +0100

    Fix import errors after removing module.
    - Remove import of removed module highlight_item.py
    - Remove import of highlight_item in test. Change test implementation to
      still work.

--- src/dashboard/components/__init__.py
+++ src/dashboard/components/__init__.py
@@ -1,3 +1,2 @@
 from dashboard.components.experimental_component import experimental_component
-from dashboard.components.highlight_item import highlight_item
 from dashboard.components.navbar_component import navbar_component as navbar
