9e6c0718a73bd3d056fb075aec7552ac018b15b1
woshyy <christopher.waatz@gmail.com>
2023-03-06 09:10:16 +0100

    started to restructure the code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import pytest
+from . import helper_test_functions
+
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="")
     parser.addoption("--speedmult", action="store", default="1")
