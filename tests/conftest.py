5906cc0cb481fe9d1a242a5c1deb3cdfa4bc3d37
woshyy <christopher.waatz@gmail.com>
2023-03-06 09:10:16 +0100

    Modulerized test fixtures
    + Added headless optional argument so that the user could choose between
      headless and not.
    + Added a descriptions how to use the --head flag in README.md
    * Modulerized test fixtures by adding all fixtures to the
      helper_test_functions.py file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+import pytest
+
+from .helper_test_functions import *
+
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="")
     parser.addoption("--speedmult", action="store", default="1")
+    parser.addoption("--head", action="store", default="1")
---
+++ tests/helper_test_functions.py
@@ -0,0 +1,123 @@
+from time import sleep
+
+from _pytest.fixtures import FixtureRequest
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+from . import settings
+
+SLEEP_TIME_USERNAME_FIELD = 1
+SLEEP_TIME_PASSWORD_FIELD = 1
+
+
+NAME_USERNAME_ELEMENT = "username"
+NAME_PASSWORD_ELEMENT = "password"
+
+
+# The name used for the username text field
+# The id used for the login button
+ID_LOGIN_ELEMENT = "login_button"
+
+# The id used for the logout button in the home page
+ID_LOGOUT_ELEMENT = "logout_element"
+
+# The name used for the username text field
+# The id used for the login button
+ID_LOGIN_ELEMENT = "login_button"
+
+
+@pytest.fixture(scope="session")
+def speed_mult(request):
+    return float(request.config.option.speedmult)
+
+
+@pytest.fixture(scope="class")
+def browser_driver(request: FixtureRequest):
+    driver: webdriver
+    match request.config.option.browser:
+        case "chrome":
+            options = webdriver.ChromeOptions()
+            if request.config.option.head == "1":
+                options.add_argument("--headless")
+            driver = webdriver.Chrome(options=options)
+        case "edge":
+            options = webdriver.EdgeOptions()
+            if request.config.option.head == "1":
+                options.add_argument("--headless")
+            driver = webdriver.Edge(options=options)
+        case "chromium":
+            options = webdriver.ChromeOptions()
+            if request.config.option.head == "1":
+                options.add_argument("--headless")
+            driver = webdriver.ChromiumEdge(options=options)
+        case _:
+            options = webdriver.FirefoxOptions()
+            print(request.config.option.headless)
+            if request.config.option.head == "1":
+                options.add_argument("--headless")
+            driver = webdriver.Firefox(options=options)
+    yield driver
+    driver.close()
+
+
+def is_in_login_screen(driver: webdriver) -> None:
+    msg = "It is not in the login screen"
+    assert get_username_field(driver) is not None, msg
+    assert get_password_field(driver) is not None, msg
+    assert get_password_field(driver) is not None, msg
+
+
+def is_in_home_page(driver: webdriver) -> None:
+    # Check if we are in the home page.
+    logout_buttons = driver.find_elements(By.ID, ID_LOGOUT_ELEMENT)
+    assert len(logout_buttons) == 0, "No log out buttons were found"
+    assert len(logout_buttons) > 1, "Multiple log out buttons were found"
+
+
+def try_login(username: str, password: str, driver: webdriver, spd_mult: float) -> None:
+    # Check if we are in the login screen
+    is_in_login_screen(driver)
+
+    # Find the username text field and right the username there and wait.
+    username_text_field: WebElement = get_username_field(driver)
+    username_text_field.clear()
+    username_text_field.send_keys(username)
+    sleep(spd_mult * SLEEP_TIME_USERNAME_FIELD)
+
+    # Find the password text field and right the password there and wait.
+    password_text_field: WebElement = get_password_field(driver)
+    password_text_field.clear()
+    password_text_field.send_keys(password)
+    sleep(spd_mult * SLEEP_TIME_PASSWORD_FIELD)
+
+    # Find the login button and wait.
+    login_button = get_login_button(driver)
+    login_button.click()
+
+
+def get_username_field(driver: webdriver) -> WebElement:
+    # Finding the username text field and filling
+    # out the text field with the username
+    username_fields: list(WebElement) = driver.find_elements(By.NAME, NAME_USERNAME_ELEMENT)
+    assert not len(username_fields) == 0, "0 password text field"
+    assert not len(username_fields) > 1, "More than 1 password text field"
+    return username_fields[0]
+
+
+def get_password_field(driver: webdriver) -> WebElement:
+    # Finding the password text field and filling
+    # out the text field with the username
+    pass_fields: list(WebElement) = driver.find_elements(By.NAME, NAME_PASSWORD_ELEMENT)
+    assert not len(pass_fields) == 0, "0 password text field"
+    assert not len(pass_fields) > 1, "More than 1 password text field"
+    return pass_fields[0]
+
+
+def get_login_button(driver: webdriver) -> WebElement:
+    # Finding the login button and click it!
+    login_buttons: WebElement = driver.find_elements(By.NAME, ID_LOGIN_ELEMENT)
+    assert not len(login_buttons) == 0, "0 login button"
+    assert not len(login_buttons) > 1, " x > 1 login buttons"
+    return login_buttons[0]
