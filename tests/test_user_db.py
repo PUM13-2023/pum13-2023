cf24cf173f4125586d82292f1af1217c6b1171f8
Axel Matstoms <axel.matstoms@gmail.com>
2023-03-07 15:54:07 +0100

    Update tests for user model
    According to the SRS, no register function needs to exist. Therefore the
    tests were updated to assume that user that do not exist are created
    upon login. This commit adds tests that test that functionality and
    remove the old tests that assumed a separate register function.

--- tests/test_user_db.py
+++ tests/test_user_db.py
@@ -1,8 +1,8 @@
-import pytest
 import mongoengine
+import pytest
 
-from dashboard.models import user
-from dashboard.models import db
+from dashboard.models import db, user
+
 
 @pytest.fixture
 def connection(autouse=True):
@@ -16,42 +16,35 @@ def connection(autouse=True):
 def clear_database(connection):
     connection.user.drop()
 
+
 @pytest.fixture
 def example_user():
     username = "fixture-user"
-    return user.register_user(username)
+    return user.login_user(username)
 
 
 def find_user(username):
     return user.User.objects(username=username).first()
 
 
-def test_register_user():
-    username = "test-register-user"
-    u = user.register_user(username)
-    assert u is not None
+def test_login_new_user():
+    username = "test-new-user"
 
-    assert find_user(username) is not None
+    usr = user.login_user(username)
 
-
-def test_register_twice():
-    username = "test-register-twice"
-
-    user.register_user(username)
-
-    with pytest.raises(ValueError):
-        user.register_user(username)
+    assert usr is not None
+    assert usr.username == username
 
 
-def test_login(example_user):
-    username = example_user.username
+def test_login_new_user_in_db():
+    username = "test-new-user-in-db"
 
-    u = user.login_user(username)
-    assert u is not None
-    assert u.username == username
+    usr = user.login_user(username)
+
+    assert find_user(username).id == usr.id
 
 
-def test_login_non_existant_user():
-    username = "this-user-doesnt-exist"
-    with pytest.raises(ValueError):
-        u = user.login_user(username)
+def test_login_existing_user(example_user):
+    usr = user.login_user(example_user.username)
+
+    assert usr.id == example_user.id
