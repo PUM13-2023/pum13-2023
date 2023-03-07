b898353c8f1b658c088782868b3d9181b7d92b09
robin <drken_robin@hotmail.com>
2023-03-07 16:10:33 +0100

    Code skeleton added for index
    Added empty callback function that will be used to toggle the pop-up of
    creating dashboards
    
    Added a default style for testing

--- src/dashboard/pages/index/index.py
+++ src/dashboard/pages/index/index.py
@@ -1,16 +1,16 @@
 import dash
 from dash import html
 from dash.dependencies import Component
-from dashboard.components import create_dashboard_menu
 
 dash.register_page(__name__, path="/")
 
+default_style = "bg-[#d2d2d2] transition duration-500 ease-in-out"
+
 
 def layout() -> Component:
-    return html.Div(
-        children=[
-            create_dashboard_menu.dashboard_menu()
-        ]
-    )
+    return html.Div()
 
 
+@dash.callback()
+def toggle_create_dashboard_menu(n_clicks: int) -> str:
+    return ""
