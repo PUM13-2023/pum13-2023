69efd7cd4f5bb71498c651988f0e0cac4db8a09b
woshyy <christopher.waatz@gmail.com>
2023-03-08 15:36:39 +0100

    Change to using WebDriverWait and removed --speedmult flag
    * Change time.sleep() to WebDriverWait() because WebDriverWait can wait
      until a specific conditions is met.
    - Removed speedmult flag because it not needed anymore because we
      changed to using WebDriverWait()
    - Removed the entry about the --speedmult flag from the test README.md

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,4 @@ from .helper_test_functions import *
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="")
-    parser.addoption("--speedmult", action="store", default="1")
     parser.addoption("--head", action="store", default="1")
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,9 @@
-from time import sleep
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 
 from . import helper_test_functions as helper
 from . import settings
@@ -14,34 +14,35 @@ WRONG_USERNAME = "not_valid_user"
 WRONG_PASSWORD = "not_valid_user_password"
 
 SLEEP_TIME_LOGIN_BUTTON = 2
-SLEEP_TIME_REFRESH = 2
+TIMEOUT_REFRESH = 2
 
 ID_POP_UP_ELEMENT = "login_error_popup"
+TIMEOUT_POPUP = 3
 
 
 @pytest.mark.test_login
 class TestLogin:
     @pytest.mark.test_unsuccessful_login
-    @pytest.mark.usefixtures("browser_driver", "speed_mult")
-    def test_unsuccessful_login(self, browser_driver: webdriver, speed_mult: float):
+    @pytest.mark.usefixtures("browser_driver")
+    def test_unsuccessful_login(self, browser_driver: webdriver):
         browser_driver.get(settings.START_PAGE_URL)
         # Try logging in with wrong password and username
-        helper.try_login(WRONG_USERNAME, WRONG_PASSWORD, browser_driver, speed_mult)
+        helper.try_login(WRONG_USERNAME, WRONG_PASSWORD, browser_driver)
         self.check_login_error_pop_up(browser_driver)
 
         # Try logging in with wrong password
-        helper.try_login(settings.USERS_USERNAME, WRONG_PASSWORD, browser_driver, speed_mult)
+        helper.try_login(settings.USERS_USERNAME, WRONG_PASSWORD, browser_driver)
         self.check_login_error_pop_up(browser_driver)
 
         # Try logging in with wrong username
-        helper.try_login(WRONG_USERNAME, settings.USERS_PASSWORD, browser_driver, speed_mult)
+        helper.try_login(WRONG_USERNAME, settings.USERS_PASSWORD, browser_driver)
         self.check_login_error_pop_up(browser_driver)
 
     @pytest.mark.test_successful_login
-    @pytest.mark.usefixtures("browser_driver", "speed_mult")
-    def test_successfull_login(self, browser_driver: webdriver, speed_mult: float):
+    @pytest.mark.usefixtures("browser_driver")
+    def test_successfull_login(self, browser_driver: webdriver):
         browser_driver.get(settings.START_PAGE_URL)
-        helper.try_login(settings.USERS_USERNAME, settings.USERS_PASSWORD, browser_driver, float)
+        helper.try_login(settings.USERS_USERNAME, settings.USERS_PASSWORD, browser_driver)
         # Check that we are still in the homepage after login in
         helper.is_in_home_page(browser_driver)
 
@@ -51,16 +52,13 @@ class TestLogin:
         helper.is_in_home_page(browser_driver)
 
     @pytest.mark.test_logout
-    @pytest.mark.usefixtures("browser_driver", "speed_mult")
-    def test_logout(self, browser_driver: webdriver, speed_mult: float):
+    @pytest.mark.usefixtures("browser_driver")
+    def test_logout(self, browser_driver: webdriver):
         browser_driver.get(settings.START_PAGE_URL)
-        helper.try_login(
-            settings.USERS_USERNAME, settings.USERS_PASSWORD, browser_driver, speed_mult
-        )
+        helper.try_login(settings.USERS_USERNAME, settings.USERS_PASSWORD, browser_driver)
 
         # Check that we are still in the homepage after logging in
         helper.is_in_home_page(browser_driver)
-        sleep(self.spd_mult * SLEEP_TIME_REFRESH)
 
         # Press the log out button
         logout_button = helper.get_logout_button(browser_driver)
@@ -71,9 +69,16 @@ class TestLogin:
     def check_login_error_pop_up(self, driver: webdriver) -> None:
         # Check if the login error pop up exist and then
         # refresh the site and check that it does not exist.
-        pop_up_element: WebElement = driver.find_element(By.ID, ID_POP_UP_ELEMENT)
-        assert pop_up_element is not None
+        assert self.error_pop_up_exist(webdriver), "There is not any pop error that showed up"
         driver.refresh()
-        sleep(self.spd_mult * SLEEP_TIME_REFRESH)
+        assert not self.error_pop_up_exist(webdriver), "There is still a pop up after a refresh"
+
+    def error_pop_up_exist(self, driver: webdriver) -> bool:
+        # Wait until we found a the element with the pop up id.
+        WebDriverWait(driver, timeout=TIMEOUT_POPUP).until(
+            ec.presence_of_element_located(By.ID, ID_POP_UP_ELEMENT)
+        )
+
+        # Find and return the element
         pop_up_element: WebElement = driver.find_element(By.ID, ID_POP_UP_ELEMENT)
-        assert pop_up_element is None
+        return pop_up_element is not None
