3b874357551525f6ea9d254aed44f9e2e207b709
Hampus Holm <57066006+haholm@users.noreply.github.com>
2023-03-20 02:28:57 +0100

    Create mockup dashboards page * Create dashboards page that looks like figma sketch * Add test file for dashboards page

--- src/dashboard/pages/dashboards.py
+++ src/dashboard/pages/dashboards.py
@@ -8,5 +8,47 @@ dash.register_page(
 
 
 def layout() -> html.Div:
-    """Stub layout for dashboard page."""
-    return html.Div()
+    return html.Div(
+        className="flex flex-col mx-4",
+        children=[
+            html.H1(className="text-3xl my-8", children="Dashboards"),
+            html.Div(
+                className="bg-white rounded-full h-10 flex items-center pl-8 text-gray-600",
+                children="Search placeholder...",
+            ),
+            html.Div(
+                className="flex justify-between my-4",
+                children=[
+                    html.Button(className="bg-white", children="View"),
+                    html.Button(className="bg-white", children="Add Dashboard"),
+                ],
+            ),
+            html.Div(
+                className="bg-white",
+                children=[
+                    html.Div(
+                        className="[&>span]:flex-1 [&>span]:border-r-2 [&>span]:ml-2 flex justify-start border-b-2 border-black text-lg",
+                        children=[
+                            html.Span("Title", className="pl-6"),
+                            html.Span("Last edited at"),
+                            html.Span("Created at"),
+                        ],
+                    ),
+                    html.Div(
+                        className="[&>span]:flex-1 [&>span]:border-r-2  [&>span]:ml-2 flex justify-start items-center border-b-2 border-gray-400 text-base",
+                        children=[
+                            html.I("analytics", className="material-symbols-rounded"),
+                            html.Span(
+                                className="flex items-center",
+                                children=[
+                                    "Dashboard 1",
+                                ],
+                            ),
+                            html.Span("Today"),
+                            html.Span("Yesterday"),
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    )
---
+++ tests/test_dashboards_page.py
@@ -0,0 +1,3 @@
+class TestDashboardsPage:
+    def test_fun(self) -> None:
+        assert True
