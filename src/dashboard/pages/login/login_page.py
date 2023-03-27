245b121accaed8004a9f6bb3dcb11a5b33fd1587
ElfinSci <124572813+ElfinSci@users.noreply.github.com>
2023-03-27 09:40:18 +0200

    Created a simple login-page
    Created a simple login-page using tailwind graphical elements. If succesful login
    redirects to /main page.

--- src/dashboard/pages/login/login_page.py
+++ src/dashboard/pages/login/login_page.py
@@ -8,13 +8,13 @@
 PORT = 8000
 ADDRESS = "127.0.0.1"
 
+from gc import callbacks
+
 import dash
-from dash import Dash, dcc, html
-from dash.dependencies import Component, Input, Output
+from dash import Dash, callback, dcc, html
+from dash.dependencies import Component, Input, Output, State
 
 dash.register_page(__name__, path="/login")
-
-image_path = "assets/logoTransparent.png"
 
 
 # sets the colors of the login page
@@ -27,42 +27,38 @@ colors = {
     "black": "#00000",
 }
 
-# allowed input
-allowed_input = (
-    "text",
-    "number",
-    "password",
-)
 
-
+# the main graphical component for the entire login page
 def layout() -> Component:
-    return html.Div(  # h-full should be used but, h-screen gives a full but scrollable background
+    return html.Div(
         className=f'bg-[{colors["background"]}] flex h-screen w-full justify-center items-center',
         children=[
             # Layout for the login meny
             # main rectangle
             html.Div(
+                id="login_ui",
                 className="flex h-[30%] w-[60%]  ",
                 children=[
                     # left rectangle
                     html.Div(
-                        className=f'bg-[{colors["meny_back"]}] flex flex-col items-center justify-center w-[50%] rounded-l-lg',
+                        className=f'bg-[{colors["meny_back"]}] flex flex-col items-center'
+                        " justify-center w-[50%] rounded-l-lg",
                         children=[
-                            # input fields
+                            # used id input field
                             dcc.Input(
                                 className="w-[60%] mb-5 px-3 py-1 rounded-full",
-                                id="userid",
+                                id="username",
                                 type="text",
                                 placeholder="username",
                             ),
-                            # password field, input is not aken
+                            # password input field
                             dcc.Input(
                                 className="w-[60%] mb-5 px-3 py-1 rounded-full",
                                 id="password",
-                                type="text",
+                                type="password",
                                 placeholder="password",
                             ),
-                            # Login button, input not taken
+                            # login button
                             html.Button(
                                 className=f'bg-[{colors["dark_purp"]}] pd-4 w-[30%] rounded-full',
                                 children=[
@@ -73,7 +69,7 @@ def layout() -> Component:
                                         ],
                                     )
                                 ],
-                                id="verify",
+                                id="login_button",
                                 n_clicks=0,
                             ),
                             html.Div(id="output1"),
@@ -81,17 +77,18 @@ def layout() -> Component:
                     ),
                     # right rectangle
                     html.Div(
-                        className=f'bg-[{colors["white"]}] flex flex-col items-center justify-center w-[50%] rounded-r-lg',
+                        className=f'bg-[{colors["white"]}] flex flex-col items-center'
+                        " justify-center w-[50%] rounded-r-lg",
                         children=[
                             html.H1(
-                                className=f" mt-4",
+                                className=f" mt-4 pd-4 ",
                                 children=[
                                     "Welcome to GraphIt",
                                 ],
                             ),
                             # product logo
                             html.Img(
-                                src=image_path,
+                                src=dash.get_asset_url("logoTransparent.png"),
                             ),
                         ],
                     ),
@@ -101,37 +98,20 @@ def layout() -> Component:
     )
 
 
-# @main.callback(
-#   dash.dependencies.Output("output1", "children"),
-#  [dash.dependencies.Input("verify", "n_clicks")],
-# state=[State("user", "value"), State("passw", "value")],
-# )
-def update_output(n_clicks, uname, passw):
-    li = {"admin123": "admin123"}
-    if uname == "" or uname == None or passw == "" or passw == None:
-        return html.Div(children="", style={"padding-left": "550px", "padding-top": "10px"})
-    if uname not in li:
-        return html.Div(
-            children="Incorrect Username",
-            style={"padding-left": "550px", "padding-top": "40px", "font-size": "16px"},
-        )
-    if li[uname] == passw:
-        return html.Div(
-            dcc.Link(
-                "Access Granted!",
-                href="/next_page",
-                style={
-                    "color": "#183d22",
-                    "font-family": "serif",
-                    "font-weight": "bold",
-                    "text-decoration": "none",
-                    "font-size": "20px",
-                },
-            ),
-            style={"padding-left": "605px", "padding-top": "40px"},
-        )
-    else:
-        return html.Div(
-            children="Incorrect Password",
-            style={"padding-left": "550px", "padding-top": "40px", "font-size": "16px"},
-        )
+# takes the input from the div that contains the input for username, password and login button
+@callback(
+    Output("output1", "children"),
+    Input("login_button", "n_clicks"),
+    State("username", "value"),
+    State("password", "value"),
+)
+
+
+# is the loginfunction that checks if a user can login when the login button is pressed.
+# currently username: "admin" and login: "admin"
+def update_login(n_clicks, username, password):
+    if username == "admin" and password == "admin":
+        # return f"You selected: {input_value}"
+        return dcc.Location(pathname="/main", id="id1")
+    if n_clicks > 0:
+        return f"Wrong Input!"
