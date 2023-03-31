2ac1e82b394b9f4153b9d06d66465dd5f4549b03
robin <drken_robin@hotmail.com>
2023-03-31 16:41:14 +0200

    Used icon and clear up
    Started using the icon component.
    
    Removed search bar test div.
    
    Added the button list to an ordered dict instead to have its name
    correspond to its correct icon on google.

--- src/dashboard/pages/graphs.py
+++ src/dashboard/pages/graphs.py
@@ -1,47 +1,35 @@
+from collections import OrderedDict
+
 import dash
-from dash import html, get_asset_url
+from dash import get_asset_url, html
+
+from dashboard.components.icon import icon
 
 dash.register_page(__name__, path_template="/dashboards/<dashboard_id>")
 
-buttons = [
-    "Presentation view",
-    "Share",
-    "Add graph"
-]
+# Ordered dict containing the name of the button and its corresponding icon name
+# Find the icon names here https://fonts.google.com/icons?icon.set=Material+Symbols
+buttons = OrderedDict(
+    [("Presentation view", "present_to_all"), ("Share", "share"), ("Add graph", "add")]
+)
 
-def search_bar():
-    bar = html.Div(
-        className="flex",
-        children=[
-            html.Div(
-                className="w-40",
-                children=["search"]
-            ),
-            html.Div(
-                "bar"
-            )
-        ]
-    )
-    return bar
 
-def add_buttons(button_names: list[str]) -> list[html.Button]:
+def add_buttons(button_names: OrderedDict[str, str]) -> list[html.Button]:
     buttons: list[html.Button] = []
 
-    for item_name in button_names:
+    for name, icon_ in button_names.items():
         buttons.append(
             html.Button(
-            className="flex items-center mx-2 border-2 border-black p-2 rounded-lg ripple",
-                children=[
-                    html.Img(className="w-[24px] h-[24px] mr-2",src=get_asset_url("share.svg")),
-                    html.P(item_name)
-                ]
+                className="flex items-center mx-2 border-2 p-2 rounded-lg drop-shadow-lg transition ease-in-out hover:text-white hover:bg-[#777df2]",
+                children=[icon(icon_, className="mr-2"), html.P(name)],
             )
         )
-    
+
     return buttons
 
+
 def layout(dashboard_id=None):
-    dashboard_name = f'Dashboard {dashboard_id}'
+    dashboard_name = f"Dashboard {dashboard_id}"
     """
     TODO
         Use the dashboard_id to query
@@ -54,13 +42,9 @@ def layout(dashboard_id=None):
             html.Div(
                 className="flex w-screen p-5 items-center",
                 children=[
-                    html.H1(className="text-3xl",children=[dashboard_name]),
-                    html.Div(
-                        className="flex ml-auto",
-                        children=add_buttons(buttons)
-                    ),
-                    search_bar()
-                ]
+                    html.H1(className="text-3xl", children=[dashboard_name]),
+                    html.Div(className="flex ml-auto", children=add_buttons(buttons)),
+                ],
             )
-        ]
+        ],
     )
