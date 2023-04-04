fd77ce8af00f725d825ebbe554c17442cf64a130
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2023-04-04 16:47:25 +0200

    Add a modal component.
    * Adds a modal component.
    * Adds a modal container component that wraps the modal component.
    * Adds tests for modal.

--- src/dashboard/components/__init__.py
+++ src/dashboard/components/__init__.py
@@ -1,3 +1,5 @@
 from dashboard.components.button import button as button
 from dashboard.components.icon import icon as icon
+from dashboard.components.modal import modal_container as modal_container
+from dashboard.components.modal import modal_dialog as modal_dialog
 from dashboard.components.navbar_component import navbar_component as navbar
