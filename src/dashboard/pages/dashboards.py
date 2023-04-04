1e0009432861355d78804ac5f1c295e4f6fe4c04
Tobias <elfstrandtobias@gmail.com>
2023-04-04 09:42:11 +0200

    Add functionality to navbar
    + Add id to navbar for easier testing.
    + Make helper function to generate links.
    + Divide navbar into two functions, one for upper navbar, one for
      lower.
    + Implement google icon into navbar instead of SVG file, uses
      icon_name in page registry
    
    - Remove now unused SVG files

--- src/dashboard/pages/dashboards.py
+++ src/dashboard/pages/dashboards.py
@@ -2,7 +2,9 @@
 import dash
 from dash import html
 
-dash.register_page(__name__, path="/dashboards", name="Dashboards", order=1, nav_item=True)
+dash.register_page(
+    __name__, path="/dashboards", name="Dashboards", order=1, nav_item=True, icon_name="dashboard"
+)
 
 
 def layout() -> html.Div:
