f15938afb80663341f6452a06e2cf742cf65abba
Axel Matstoms <axel.matstoms@gmail.com>
2023-04-05 13:33:18 +0200

    Fix loading bug in icon component
    Due to icons using ligature fonts, the icon component had incorrect
    width during loading, leading to unexpected behaviour.
    
    To fix this, setting icon size also sets max width. To accomplish this
    in a simple manner, the interface was updated so that size is passed as
    a parameter instead of as a class name.

--- src/dashboard/components/icon.py
+++ src/dashboard/components/icon.py
@@ -25,7 +25,7 @@ def icon_style(fill: int, weight: int, grade: int) -> dict[str, str]:
 
 
 def icon(
-    icon_name: str, fill: int = 0, weight: int = 400, grade: int = 0, **kwargs: Any
+    icon_name: str, fill: int = 0, weight: int = 400, grade: int = 0, size: int = 48, **kwargs: Any
 ) -> Component:
     """Return an icon component.
 
@@ -36,6 +36,7 @@ def icon(
         weight (int): Font weight for the icon, range 100-700,
             default 400.
         grade (int): Affects font weight, range -25-200, default 0.
+        size (int): Icon size in pixels, default 48.
         kwargs (Any): Forwarded to html.Span.
 
     Examples:
@@ -49,10 +50,10 @@ def icon(
 
         Setting icon size::
 
-            icon("home", className="text-[40px]")
+            icon("home", size=24)
     """
     class_name = kwargs.pop("className", "")
-    class_name += " material-symbols-outlined"
+    class_name += f" material-symbols-outlined text-[{size}px] max-w-[{size}px]"
 
     return html.Span(
         className=class_name,
