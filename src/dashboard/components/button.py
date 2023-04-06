7d66383f2b34943b43965801240706caf51269a6
Hampus Holm <57066006+haholm@users.noreply.github.com>
2023-04-06 16:27:12 +0200

    Add kwargs to button component
    Make it possible to add custom className, id etc. attributes to the button component.

--- src/dashboard/components/button.py
+++ src/dashboard/components/button.py
@@ -1,5 +1,5 @@
 """Button component."""
-from typing import Dict
+from typing import Any
 
 from dash import html
 
@@ -13,23 +13,32 @@ def button(
     weight: int = 400,
     grade: int = 0,
     size: int = 16,
-    _id: str | Dict[str, str] = "",
+    **kwargs: Any,
 ) -> html.Button:
     """Create a general button containing an icon to the left.
 
     Args:
-        icon_name (str): The name of the icon to use.
+        icon_name (str): the name of the icon, for more information see:
+            https://fonts.google.com/icons?icon.set=Material+Symbols
         text (str): The text to be displayed inside the button.
-        size (int): The font size in pixels, also sets icon size.
+        fill (int): Set to 0 for outlined, 1 for filled. Default 0.
+        weight (int): Font weight for the icon, range 100-700,
+            default 400.
+        grade (int): Affects font weight, range -25-200, default 0.
+        size (int, optional): The font size in pixels, also sets icon
+        size. Defaults to 16
+        kwargs (Any): Forwarded to html.Button.
 
     Returns:
         html.Button: The button.
     """
+    class_name = kwargs.pop("className", "")
+
     return html.Button(
-        id=_id,
+        **kwargs,
         className=(
-            f"flex bg-white items-center text-[{size}px] px-2 py-1 rounded-lg drop-shadow-sm"
-            " hover:drop-shadow-md transition"
+            f"{class_name} flex bg-white items-center text-[{size}px] px-2 py-1 rounded-lg"
+            " drop-shadow-sm hover:drop-shadow-md transition"
         ),
         children=[
             icon(icon_name, fill=fill, weight=weight, grade=grade, size=size + 10),
