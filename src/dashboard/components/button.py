fb2b1cf4a1e6870e614de3a852cffd4455863c0a
Hampus Holm <57066006+haholm@users.noreply.github.com>
2023-04-07 21:19:39 +0200

    Type button args for icon component as TypedDict
    Created TypedDict class without totality declaring optional arguments passable to the icon component inside the button component.

--- src/dashboard/components/button.py
+++ src/dashboard/components/button.py
@@ -1,18 +1,24 @@
 """Button component."""
-from typing import Any
+from typing import Any, TypedDict
 
 from dash import html
 
 from dashboard.components.icon import icon
 
 
+class IconKWArgs(TypedDict, total=False):
+    """Class for typing icon kwargs passed to the button component."""
+
+    fill: int
+    weight: int
+    grade: int
+
+
 def button(
     icon_name: str,
     text: str,
-    fill: int = 0,
-    weight: int = 400,
-    grade: int = 0,
     size: int = 16,
+    icon_kwargs: IconKWArgs | None = None,
     **kwargs: Any,
 ) -> html.Button:
     """Create a general button containing an icon to the left.
@@ -21,18 +27,17 @@ def button(
         icon_name (str): the name of the icon, for more information see:
             https://fonts.google.com/icons?icon.set=Material+Symbols
         text (str): The text to be displayed inside the button.
-        fill (int): Set to 0 for outlined, 1 for filled. Default 0.
-        weight (int): Font weight for the icon, range 100-700,
-            default 400.
-        grade (int): Affects font weight, range -25-200, default 0.
         size (int, optional): The font size in pixels, also sets icon
-        size. Defaults to 16
+            size to size + 10. Defaults to 16
+        icon_kwargs (IconKWArgs, optional): Forwarded to icon. For more
+            information see the icon component docstring.
         kwargs (Any): Forwarded to html.Button.
 
     Returns:
         html.Button: The button.
     """
     class_name = kwargs.pop("className", "")
+    _icon_kwargs: IconKWArgs = icon_kwargs if icon_kwargs else {}
 
     return html.Button(
         **kwargs,
@@ -41,7 +46,7 @@ def button(
             " drop-shadow-sm hover:drop-shadow-md transition"
         ),
         children=[
-            icon(icon_name, fill=fill, weight=weight, grade=grade, size=size + 10),
+            icon(icon_name, size=size + 10, **_icon_kwargs),
             html.Span(className="leading-none pl-1", children=text),
         ],
     )
