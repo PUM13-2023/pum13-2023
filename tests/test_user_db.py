dec1f0f040d99bfbb732bfc6945397f054cb729f
Axel Matstoms <axel.matstoms@gmail.com>
2023-04-19 00:23:11 +0200

    Add docstrings and marker to user db tests.

--- tests/test_user_db.py
+++ tests/test_user_db.py
@@ -1,3 +1,4 @@
+"""Test user db."""
 import mongoengine
 import mongomock
 import pytest
@@ -7,46 +8,49 @@ from dashboard.models import user
 
 @pytest.fixture(autouse=True)
 def connection():
+    """Connect mongoengine to mongomock and return client."""
     conn = mongoengine.connect(
-            host="mongodb://localhost", name="dashboard", mongo_client_class=mongomock.MongoClient
+        db="dashboard", host="mongodb://localhost", mongo_client_class=mongomock.MongoClient
     )
 
     return conn
 
 
 @pytest.fixture
-def clear_database(connection):
-    connection.user.drop()
-
-
-@pytest.fixture
 def example_user():
+    """Example user."""
     username = "fixture-user"
     return user.login_user(username)
 
 
 def find_user(username):
+    """Find user by username."""
     return user.User.objects(username=username).first()
 
 
-def test_login_new_user():
-    username = "test-new-user"
+@pytest.mark.test_user_db
+class TestUserDb:
+    """Contains tests for testing user db."""
 
-    usr = user.login_user(username)
+    def test_login_new_user(self):
+        """Test login new user."""
+        username = "test-new-user"
 
-    assert usr is not None
-    assert usr.username == username
+        usr = user.login_user(username)
 
+        assert usr is not None
+        assert usr.username == username
 
-def test_login_new_user_in_db():
-    username = "test-new-user-in-db"
+    def test_login_new_user_in_db(self):
+        """Test user is stored in db."""
+        username = "test-new-user-in-db"
 
-    usr = user.login_user(username)
+        usr = user.login_user(username)
 
-    assert find_user(username).id == usr.id
+        assert find_user(username).id == usr.id
 
+    def test_login_existing_user(self, example_user):
+        """Test login returns same user if username in use."""
+        usr = user.login_user(example_user.username)
 
-def test_login_existing_user(example_user):
-    usr = user.login_user(example_user.username)
-
-    assert usr.id == example_user.id
+        assert usr.id == example_user.id
