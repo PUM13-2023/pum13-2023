79c8ef47318faee218852c62c26d709e5d729210
robin <drken_robin@hotmail.com>
2023-04-18 15:33:57 +0200

    Style and functionality
    Fixed some style and added color line in a patch

--- src/dashboard/pages/create_graph/create_graph.py
+++ src/dashboard/pages/create_graph/create_graph.py
@@ -9,9 +9,9 @@ import io
 import json
 import os
 from typing import Any
-
+import pprint
 import dash
-from dash import callback, dcc, html
+from dash import callback, dcc, html, Patch
 from dash.dependencies import Component, Input, Output, State
 from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
@@ -63,10 +63,10 @@ def layout() -> Component:
         children=[
             # dcc.Store(id="df_storage", storage_type="local"),
             dcc.Store(id="df_storage"),
-            left_setting_bar(),
             graph_window(),
             right_settings_bar(),
-            html.Plaintext(id="color_output"),
+            dcc.Input(id="graph_index", value=0, className="hidden"),
+            dcc.Input(id="num_graphs", value=0, className="hidden")
         ],
     )
 
@@ -86,10 +86,6 @@ def left_setting_bar() -> Component:
             x_axis_name(),
             y_axis_name(),
             file_name(),
-            download_png(),
-            download_jpeg(),
-            download_pdf(),
-            download_html(),
         ],
     )
 
@@ -114,32 +110,42 @@ def csv_button() -> Component:
         # True so multiple files can be uploaded
         multiple=True,
     )
+    
+def download_button(icon, text, id):
+    """Predefined button style
 
+    Args:
+        icon (_type_): _description_
+        text (_type_): _description_
+        id (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return button(icon_name=icon, text=text, id=id, className="bg-[#636af2] hover:bg-[#2F3273] justify-center flex-1 text-white")
+
+def download_buttons():
+    return html.Div(
+        className="flex flex-col",
+        children=[
+            html.P("Download as"),
+            html.Div(
+                className="flex space-x-2",
+                children=[
+                    download_button(icon="image", text="Png", id="download_png"),
+                    download_button(icon="image", text="Jpeg", id="download_jpeg"),
+                    download_button(icon="picture_as_pdf", text="Pdf", id="download_pdf"),
+                    download_button(icon="html", text="Html", id="download_html"),
+                ]
+                     
+            ),
+            
+        ]
+    )
 
 def db_button() -> Component:
     """NOT IN USE: button to get data from a database."""
     return button2("Get from database", "database_button")
-
-
-def download_png() -> Component:
-    """Called to download the created graph as png"""
-    return button2("Download as png", "download_png")
-
-
-def download_jpeg() -> Component:
-    """Called to download the created graph as jpeg"""
-    return button2("Download as jpg", "download_jpeg")
-
-
-def download_pdf() -> Component:
-    """Called to download the created graph as pdf"""
-    return button2("Download as pdf", "download_pdf")
-
-
-def download_html() -> Component:
-    """Called to download the created graph as html"""
-    return button2("Download as html", "download_html")
-
 
 def button2(button_text: str, button_id: str) -> Component:
     return html.Button(
@@ -191,7 +197,7 @@ def input_field(loc_id: str, loc_placeholder: str) -> Component:
 
     """
     return dcc.Input(
-        className=f"bg-[{colors['background']}] flex items-center justify-center mt-5 p-2 h-[30%]",
+        className=f"bg-[{colors['background']}] flex items-center justify-center mt-5 p-2 rounded-md shadow-inner",
         id=loc_id,
         type="text",
         debounce=True,
@@ -208,7 +214,11 @@ def graph_window() -> Component:
     return html.Div(
         className="bg-white w-full ml-[3rem] my-[3rem] rounded-md shadow-md",
         children=[
+            input_field("graph_name", "Graph name"),
             html.Div(id="graph_output"),
+            x_axis_name(),
+            y_axis_name(),
+            file_name(),
         ],
     )
 
@@ -219,8 +229,9 @@ def create_fig(
     # graph_name: str,
     # x_axis_name: str,
     # y_axis_name: str,
-    color_output: dict[str, str],
+    color_input: str,
     num: int,
+    id="",
 ) -> go.Figure:
     """Creates a graph based on the chosen type by the user.
 
@@ -250,16 +261,17 @@ def create_fig(
             fig = go.Scatter(
                 x=df["x"],
                 y=df["y"],
-                marker_color=color_output["color"],
+                marker_color=color_input,
                 mode="lines",
                 name="graf " + str(num),
+                uid=id
             )
 
         if graph_type == "scatter":
             fig = go.Scatter(
                 x=df["x"],
                 y=df["y"],
-                marker_color=color_output["color"],
+                marker_color=color_input,
                 mode="markers",
                 name="graf " + str(num),
             )
@@ -268,7 +280,7 @@ def create_fig(
             fig = go.Histogram(
                 # x=x1_list,
                 x=df["y"],
-                marker_color=color_output["color"],
+                marker_color=color_input,
                 name="graf " + str(num),
             )
 
@@ -296,9 +308,26 @@ def top_right_settings() -> html.Div:
             ),
             upload_buttons(),
             radio_buttons(),
+            download_buttons(),
+            color_picker()
+
         ],
     )
 
+def color_picker():
+    return html.Div(
+        className="flex flex-col",
+        children=[
+            html.Label("Line color"),
+            dbc.Input(
+                type="color",
+                id="color_input",
+                value="#0000FF",
+                style={"width": 75, "height": 50},
+                debounce=True,
+            ),
+        ]
+    )
 
 def upload_buttons() -> html.Div:
     """Buttons for uploading file.
@@ -381,23 +410,17 @@ def right_settings_bar() -> Component:
                         id="create-graph",
                         className="bg-[#636af2] px-5 py-3 hover:bg-[#2F3273]",
                     ),
-                    html.Label("Choose color for graph 1 "),
-                    dbc.Input(
-                        type="color",
-                        id="color_input",
-                        value="#0000FF",
-                        style={"width": 75, "height": 50},
-                        debounce=True,
-                    ),
                 ],
             ),
         ],
     )
 
 
-@callback(Output("color_output", "style"), Input("color_input", "value"))
+@callback(Output("graph_1", "figure"), Input("color_input", "value"))
 def choose_color1(color_input: str) -> dict[str, Any]:
-    return {"color": color_input}
+    patched_figure = Patch()
+    patched_figure["data"][0]["marker"]["color"] = color_input
+    return patched_figure
 
 
 def radio_item(name: str, value: str, icon_name: str) -> dict[str, html.Div]:
@@ -439,9 +462,14 @@ def parse_contents(contents: str) -> pl.DataFrame:
 
     return pl.read_csv(io.StringIO(decoded.decode("utf-8")))
 
+@callback(
+    Output("num_graphs", "value"),
+    Input("uploaded_data", "contents"),
+)
+def number_of_graphs(contents):
+    return len(contents)
 
 @callback(
-    # Output("df_storage", "data"),
     [Output("df_storage", "data")],
     Input("uploaded_data", "contents"),
 )
@@ -469,7 +497,16 @@ def store_dataframe(contents: str) -> tuple[list[dict[str, list[Any]]]]:
 
     except ValueError:
         raise PreventUpdate
-
+    
+@callback(
+    Output("graph_id", "figure"),
+    Input("color_input", "value"),
+)
+def patch_color(color):
+    patched_figure = Patch()
+    patched_figure["data"][0]["marker"] = {"color": color}
+    return patched_figure
+    
 
 @callback(
     Output("graph_output", "children"),
@@ -483,7 +520,7 @@ def store_dataframe(contents: str) -> tuple[list[dict[str, list[Any]]]]:
     Input("download_jpeg", "n_clicks"),
     Input("download_pdf", "n_clicks"),
     Input("download_html", "n_clicks"),
-    Input("color_output", "style"),
+    Input("graph_index", "value")
 )
 def main(
     df_storage: Component,
@@ -496,7 +533,7 @@ def main(
     download_jpeg: bool,
     download_html: bool,
     download_pdf: bool,
-    color_output: dict[str, str],
+    graph_index
 ) -> Component:
     """Main function for the code.
 
@@ -539,20 +576,20 @@ def main(
         data_frames = [pl.from_dict(x) for x in df_storage]
 
     created_figs = []
-    num = 1
-    for i in data_frames:
+    for num, i in enumerate(data_frames):
         loc_fig = create_fig(
             i,
             choose_graph_type,
             # graph_name,
             # x_axis_name,
             # y_axis_name,
-            color_output,
-            num,
+            "#000000",
+            num + 1,
+            id="graph_"+ str(num)
         )
-        num = num + 1
         created_figs.append(loc_fig)
-
+    
+    
     layout = go.Layout(
         title=graph_name,
         # to not show grid-lines
@@ -567,8 +604,10 @@ def main(
 
     fig = go.Figure(data=created_figs, layout=layout)
 
+    print(fig.to_json())
+    
     if download_png:
-        fig.write_image("graph_images/" + file_name + ".png", width=1920, height=1080)
+        fig.write_image(file=f'graph_images/{file_name}', format="png", width=1920, height=1080)
     if download_jpeg:
         fig.write_image("graph_images/" + file_name + ".jpeg", width=1920, height=1080)
     if download_pdf:
@@ -576,5 +615,5 @@ def main(
     if download_html:
         fig.write_html("graph_images/" + file_name + ".html")
 
-    loc_graph = dcc.Graph(figure=fig, config=loc_config)
+    loc_graph = dcc.Graph(figure=fig, config=loc_config, id="graph_id")
     return loc_graph
