e64228a78f748610b99e59d99344294d77c7c03a
Hampus Holm <57066006+haholm@users.noreply.github.com>
2023-04-24 15:08:57 +0200

    Add pluralize, singularize and to_human_time_delta
    Add pluralize, singularize and to_human_time_delta to utilities.py. Replace prior function in dashboards_list_component.

--- src/dashboard/components/dashboards_list_component.py
+++ src/dashboard/components/dashboards_list_component.py
@@ -1,10 +1,11 @@
 """Dashboards list component."""
 
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from dash.dependencies import Component
 
 from dashboard.models.user import Dashboard
+from dashboard.utilities import to_human_time_delta
 
 from .list_component import list_component
 
@@ -20,37 +21,10 @@ def generate_list_row_contents(dashboard: Dashboard) -> list[str]:
     """
     datetime_now = datetime.now()
 
-    def get_readable_time_delta(since: datetime) -> str:
-        time_delta: timedelta = datetime_now - since
-        seconds = int(time_delta.total_seconds())
-        days = time_delta.days
-        hours = seconds // 3600
-        minutes = seconds // 60
-        if days > 0:
-            if days >= 7:
-                # NOTE: locale will change %b !
-                return since.strftime("%d %b. %Y")
-            if days > 1:
-                return f"{days} days ago"
-
-            return "A day ago"
-        if hours > 0:
-            if hours > 1:
-                return f"{hours} hours ago"
-
-            return "An hour ago"
-        if minutes > 0:
-            if minutes > 1:
-                return f"{minutes} minutes ago"
-
-            return "A minute ago"
-
-        return f"{time_delta.seconds} seconds ago"
-
     return [
         dashboard.name,
-        get_readable_time_delta(dashboard.modified),
-        get_readable_time_delta(dashboard.created),
+        to_human_time_delta(datetime_now, dashboard.modified),
+        to_human_time_delta(datetime_now, dashboard.created),
     ]
 
 
--- src/dashboard/utilities.py
+++ src/dashboard/utilities.py
@@ -1,4 +1,5 @@
 """Module with utility functions that are reused frequently."""
+from datetime import datetime
 
 
 def set_classname(class_str: str, class_to_set: str, set_: bool) -> str:
@@ -46,3 +47,72 @@ def toggle_classname(class_str: str, class_to_toggle: str) -> str:
         return set_classname(class_str, class_to_toggle, False)
     else:
         return set_classname(class_str, class_to_toggle, True)
+
+
+def pluralize(unit: str, amount: int) -> str:
+    """Pluralize a unit if the amount is greater than 1.
+
+    Args:
+        unit (str): The unit.
+        amount (int): The amount.
+
+    Returns:
+        str: The unit with s appended
+        if amount is greater than 1,
+        otherwise the unit.
+    """
+    if amount == 1:
+        return unit
+
+    return f"{unit}s"
+
+
+def singularize(article: str, amount: int) -> str:
+    """Singularize an amount if it is equal to one.
+
+    Args:
+        article (str): The indefinite
+        article of an amount of something.
+        E.g. "A" or "An" in english.
+        amount (int): An amount.
+
+    Returns:
+        str: `article` if the amount
+        is equal to 1, otherwise
+        a string of amount.
+    """
+    if amount == 1:
+        return article
+
+    return str(amount)
+
+
+def to_human_time_delta(t1: datetime, t2: datetime) -> str:
+    """Convert a timedelta to a human readable form.
+
+    Args:
+        delta (timedelta): A duration
+        of time.
+
+    Returns:
+        str: A human readable form of
+        the delta.
+    """
+    delta = abs(t1 - t2)
+    units: list[tuple[str, str, int]] = [
+        ("A", "week", 604800),
+        ("A", "day", 86400),
+        ("An", "hour", 3600),
+        ("A", "minute", 60),
+        ("A", "second", 1),
+    ]
+    for article, unit, duration_seconds in units:
+        unit_duration = int(delta.total_seconds()) // duration_seconds
+        if unit_duration >= 1:
+            return (
+                f"{singularize(article, unit_duration)} {pluralize(unit, unit_duration)} ago"
+                if unit != "week"
+                else min(t1, t2).strftime("%d %b. %Y")
+            )
+
+    return "Just now"
