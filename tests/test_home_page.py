e73b9052b7e056b6e9584edf1b7819471622c954
robin <drken_robin@hotmail.com>
2023-05-02 13:18:39 +0200

    Add test home page modal
    Added tests for add dashboard callback.
    * Test for empty title
    * Test for empty description
    * Test for empty title and description
    * Test that title and description is filled

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -5,9 +5,11 @@ from selenium.webdriver.remote.webelement import WebElement
 from tests import helper_test_functions as helper
 from tests import settings
 
+from dashboard.pages.index import controller
+
 from .settings import DriverType
 
-CREATE_DASHBOARD_BUTTON_ID = "create-dashboard"
+CREATE_DASHBOARD_BUTTON_ID = "create-dashboard-btn"
 LATEST_OPENED_DASHBOARDS_ID = "latest-opened-dashboards"
 SHARED_DASHBOARDS_ID = "shared-dashboards"
 USERNAME = "home-page-user"
@@ -18,6 +20,10 @@ PASSWORD = "password"
 TODO
 Add tests for modals when modal component is finished
 """
+EMPTY_TITLE_OUTPUT = (True, True, False)
+EMPTY_DESC_OUTPUT = (True, False, True)
+EMPTY_TITLE_DESC_OUTPUT = (True, True, True)
+VALID_OUTPUT = (False, False, False)
 
 
 @pytest.fixture(scope="class")
@@ -33,15 +39,39 @@ class TestHomePage:
 
     @pytest.mark.usefixtures("start_server", "login_session")
     def test_create_dashboard_button(self, browser_driver: DriverType) -> None:
-        """Check that the create dashboard button exists.
+        """Test the create dashboard button.
 
         Args:
             browser_driver (DriverType): webdriver used for selenium
         """
-        browser_driver.get(settings.URL)
+        browser_driver.get(settings.HOME_PAGE_URL)
 
-        element: WebElement = helper.get_element_by_id(browser_driver, CREATE_DASHBOARD_BUTTON_ID)
-        assert element
+        create_dashboard: WebElement = helper.get_element_by_id(
+            browser_driver, CREATE_DASHBOARD_BUTTON_ID
+        )
+        create_dashboard.click()
+
+    @pytest.mark.usefixtures("start_server")
+    def test_create_dashboard(self, browser_driver: DriverType) -> None:
+        """Tries to create dashboards using callback.
+
+        Args:
+            browser_driver (DriverType): webdriver used for selenium
+        """
+        browser_driver.get(settings.HOME_PAGE_URL)
+
+        create_dashboard: WebElement = helper.get_element_by_id(
+            browser_driver, CREATE_DASHBOARD_BUTTON_ID
+        )
+        create_dashboard.click()
+        empty_title_output = controller.add_dashboard_db(0, "", "Text")
+        valid_dashboard_output = controller.add_dashboard_db(0, "Title", "Text")
+        empty_desc_output = controller.add_dashboard_db(0, "Title", "")
+        empty_title_desc_ouput = controller.add_dashboard_db(0, "", "")
+        assert empty_title_output == EMPTY_TITLE_OUTPUT
+        assert valid_dashboard_output == VALID_OUTPUT
+        assert empty_desc_output == EMPTY_DESC_OUTPUT
+        assert empty_title_desc_ouput == EMPTY_TITLE_DESC_OUTPUT
 
     @pytest.mark.usefixtures("start_server", "login_session")
     def test_carousel(self, browser_driver: DriverType) -> None:
@@ -50,6 +80,6 @@ class TestHomePage:
         Args:
             browser_driver (DriverType): webdriver used for selenium
         """
-        browser_driver.get(settings.URL)
+        browser_driver.get(settings.HOME_PAGE_URL)
         helper.get_element_by_id(browser_driver, LATEST_OPENED_DASHBOARDS_ID)
         helper.get_element_by_id(browser_driver, SHARED_DASHBOARDS_ID)
