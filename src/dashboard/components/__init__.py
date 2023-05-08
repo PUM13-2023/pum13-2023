1a66297c061a1ab899de79ccb81c01eac30bcfa8
Axel Matstoms <axel.matstoms@gmail.com>
2023-05-08 11:42:57 +0200

    Add require_login decorator
    Temporary implementation, ideally would redirect etc.

--- src/dashboard/components/__init__.py
+++ src/dashboard/components/__init__.py
@@ -1,6 +1,7 @@
-from dashboard.components.button import button as button
-from dashboard.components.dashboards_list_component import (
-    dashboards_list_component as dashboards_list_component,
-)
-from dashboard.components.icon import icon as icon
+from dashboard.components.auth import login_required
+from dashboard.components.button import button
+from dashboard.components.dashboards_list_component import dashboards_list_component
+from dashboard.components.icon import icon
 from dashboard.components.navbar_component import navbar_component as navbar
+
+__all__ = ["button", "dashboards_list_component", "icon", "navbar", "login_required"]
