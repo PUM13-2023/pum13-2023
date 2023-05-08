e590823f1048cd8927c6220edd95a40c3ba475a7
Axel Matstoms <axel.matstoms@gmail.com>
2023-05-08 21:09:29 +0200

    Update tests to work with login
    Since some behaviour changed with the implementation of login, some of
    the tests needed to be updated to match this functionality.

--- tests/test_dashboards_page.py
+++ tests/test_dashboards_page.py
@@ -6,13 +6,22 @@ from . import helper_test_functions as helper
 from . import settings
 from .settings import DriverType
 
+USERNAME = "dashboards-test-user"
+PASSWORD = "password"
+
+
+@pytest.fixture(scope="class")
+def login_session(browser_driver: DriverType) -> None:
+    """Log in session."""
+    browser_driver.get(settings.START_PAGE_URL)
+    helper.try_login(browser_driver, USERNAME, PASSWORD)
+
 
 @pytest.mark.test_dashboards_page
 class TestDashboardsPage:
     """Class that contains tests for the dashboards page."""
 
-    @pytest.mark.usefixtures("start_server")
-    @pytest.mark.usefixtures("browser_driver")
+    @pytest.mark.usefixtures("start_server", "login_session")
     def test_find_dashboards_list(self, browser_driver: DriverType) -> None:
         """Test that the dashboards page list can be found.
 
@@ -22,8 +31,7 @@ class TestDashboardsPage:
         browser_driver.get(settings.DASHBOARDS_PAGE_URL)
         helper.get_element_by_id(browser_driver, "dashboards-list")
 
-    @pytest.mark.usefixtures("start_server")
-    @pytest.mark.usefixtures("browser_driver")
+    @pytest.mark.usefixtures("start_server", "login_session")
     def test_add_new_dashboard(self, browser_driver: DriverType) -> None:
         """Test adding a new dashboard.
 
--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -10,6 +10,8 @@ from .settings import DriverType
 CREATE_DASHBOARD_BUTTON_ID = "create-dashboard"
 LATEST_OPENED_DASHBOARDS_ID = "latest-opened-dashboards"
 SHARED_DASHBOARDS_ID = "shared-dashboards"
+USERNAME = "home-page-user"
+PASSWORD = "password"
 
 
 """
@@ -18,11 +20,18 @@ Add tests for modals when modal component is finished
 """
 
 
+@pytest.fixture(scope="class")
+def login_session(browser_driver: DriverType) -> None:
+    """Log in session."""
+    browser_driver.get(settings.START_PAGE_URL)
+    helper.try_login(browser_driver, USERNAME, PASSWORD)
+
+
 @pytest.mark.test_home_page
 class TestHomePage:
     """A class to group functions to test the dashboard capabilities."""
 
-    @pytest.mark.usefixtures("start_server")
+    @pytest.mark.usefixtures("start_server", "login_session")
     def test_create_dashboard_button(self, browser_driver: DriverType) -> None:
         """Check that the create dashboard button exists.
 
@@ -34,7 +43,7 @@ class TestHomePage:
         element: WebElement = helper.get_element_by_id(browser_driver, CREATE_DASHBOARD_BUTTON_ID)
         assert element
 
-    @pytest.mark.usefixtures("start_server")
+    @pytest.mark.usefixtures("start_server", "login_session")
     def test_carousel(self, browser_driver: DriverType) -> None:
         """Check that the carousels are displayed.
 
