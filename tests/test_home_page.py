18f6305889724bf9b290bf49c2b6e090f37eb624
robin <drken_robin@hotmail.com>
2023-05-11 12:37:50 +0200

    Added login to tests
    Added login session for the tests.

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,5 +1,8 @@
 """Test home page."""
 
+from flask import Flask
+from flask.ctx import RequestContext
+from flask_login import LoginManager
 import mongoengine
 import mongomock
 import pytest
@@ -8,6 +11,7 @@ from tests import helper_test_functions as helper
 from tests import settings
 
 from dashboard.models import user
+from dashboard.models.user import User, login_user
 from dashboard.pages.index import controller
 
 from .settings import DriverType
@@ -50,12 +54,41 @@ def connection():
     return conn
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def example_user():
     """Example user."""
     username = "dashboards-page-test-user"
     usr = user.login_user(username)
     return usr
+
+
+@pytest.fixture
+def app() -> Flask:
+    """Return a Flask app."""
+    app = Flask(__name__)
+    app.secret_key = "test-key123"
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id: str) -> User:
+        """Load a user by id.
+
+        Required by flask-login. For more information, see
+        https://flask-login.readthedocs.io/en/latest/#your-user-class
+        """
+        user: User = User.objects(id=user_id).get()
+
+        return user
+
+    return app
+
+
+@pytest.fixture
+def ctx(app: Flask) -> RequestContext:
+    """Yield a flask request context."""
+    with app.test_request_context() as ctx:
+        yield ctx
 
 
 @pytest.mark.test_home_page
@@ -78,12 +111,16 @@ class TestHomePage:
         dashboard_modal: WebElement = helper.get_element_by_id(browser_driver, DASHBOARD_MODAL_ID)
         assert "hidden" not in dashboard_modal.get_attribute("class")
 
-    def test_create_dashboard(self) -> None:
+    @pytest.mark.usefixtures("start_server", "login_session")
+    def test_create_dashboard(self, ctx: RequestContext, example_user: User) -> None:
         """Tries to create dashboards using callback.
 
         Args:
             browser_driver (DriverType): webdriver used for selenium
+            ctx (RequestContext): flask context
+            example_user (User): User model to login
         """
+        login_user(example_user.username)
         empty_title_output = controller.add_dashboard_db(0, "", "Text")
         valid_dashboard_output = controller.add_dashboard_db(0, "Title", "Text")
         empty_desc_output = controller.add_dashboard_db(0, "Title", "")
